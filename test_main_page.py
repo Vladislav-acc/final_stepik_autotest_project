a41bd106b16ef45a0880cf65a2abc23093ceaed1
vlados <vladyurkovof96@gmail.com>
2021-11-24 20:19:55 +0300

    upgraded test_main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,11 @@
-def test_guest_can_go_to_login_page(driver):
-    link = "http://selenium1py.pythonanywhere.com/"
-    driver.get(link)
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+def go_to_login_page(driver):
     login_link = driver.find_element_by_css_selector("#login_link")
     login_link.click()
+
+
+def test_guest_can_go_to_login_page(driver):
+    driver.get(link)
+    go_to_login_page(driver)
