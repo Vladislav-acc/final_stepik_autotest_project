4a3f742169f1eb403db79de14a64ead79c576a37
vlados <vladyurkovof96@gmail.com>
2021-11-25 10:26:44 +0300

    first working test with pageobject complete

--- test_main_page.py
+++ test_main_page.py
@@ -1,11 +1,8 @@
-link = "http://selenium1py.pythonanywhere.com/"
+from .pages.main_page import MainPage
 
 
-def go_to_login_page(driver):
-    login_link = driver.find_element_by_css_selector("#login_link")
-    login_link.click()
-
-
-def test_guest_can_go_to_login_page(driver):
-    driver.get(link)
-    go_to_login_page(driver)
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
